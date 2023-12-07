86463be52a288989d05a79898b973e81b04aadf4
rijacklin <rijacklin1@gmail.com>
2023-12-06 21:23:14 -0500

    Fix formatting

--- chapter02/insertion_sort.cc
+++ chapter02/insertion_sort.cc
@@ -1,15 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int *insertion_sort(int *arr, int n)
-{
-	for (int i = 0; i < n; i++)
-	{
+int *insertion_sort(int *arr, int n) {
+	for (int i = 0; i < n; i++) {
 		int key = arr[i + 1];
 		int j = i;
 
-		while (j >= 0 && arr[j] > key)
-		{
+		while (j >= 0 && arr[j] > key) {
 			arr[j + 1] = arr[j];
 			j -= 1;
 		}
@@ -20,15 +17,13 @@ int *insertion_sort(int *arr, int n)
 	return arr;
 }
 
-int main()
-{
+int main() {
 	int arr[] = {5, 2, 4, 6, 1, 3};
 	int arr_size = sizeof(arr) / sizeof(int);
 
 	int *sorted = insertion_sort(arr, arr_size);
 
-	for (int i = 0; i < arr_size; i++)
-	{
+	for (int i = 0; i < arr_size; i++) {
 		std::cout << arr[i] << std::endl;
 	}
 
