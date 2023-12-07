e662139654e493a2b84146003d689fe5559114e9
rijacklin <rijacklin1@gmail.com>
2023-12-06 21:20:18 -0500

    Updated formatting

--- chapter02/insertion_sort.cpp
+++ chapter02/insertion_sort.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int* insertion_sort(int *arr, int n) {
-	for (int i = 0; i < n; i++) {
+int *insertion_sort(int *arr, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
 		int key = arr[i + 1];
 		int j = i;
-			
-		while (j >= 0 && arr[j] > key) {
+
+		while (j >= 0 && arr[j] > key)
+		{
 			arr[j + 1] = arr[j];
 			j -= 1;
 		}
-			
+
 		arr[j + 1] = key;
 	}
 
 	return arr;
 }
 
-int main() {
-	int arr[] = { 5, 2, 4, 6, 1, 3 };
+int main()
+{
+	int arr[] = {5, 2, 4, 6, 1, 3};
 	int arr_size = sizeof(arr) / sizeof(int);
 
 	int *sorted = insertion_sort(arr, arr_size);
 
-	for (int i = 0; i < arr_size; i++) {
+	for (int i = 0; i < arr_size; i++)
+	{
 		std::cout << arr[i] << std::endl;
 	}
 
